403fe6e72c3dfe7ad40e4adb48622b827bb10fe2
mark-anthonyavila <mjavila4@miners.utep.edu>
2023-04-18 18:54:19 -0600

    Added "Favorite" page

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
+import soccer_data as sd
 import json, requests
 import subprocess
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('splash.html')
+
 
 @app.route('/teams/')
 def teamsPage():
@@ -23,5 +26,11 @@ def gamesPage():
     return render_template('games.html')
 
 
+@app.route('/favorite/')
+def favorite_page():
+    team_list = sd.getTeams(sd.header, 140)
+    return render_template('favorite.html', team_list=team_list)
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
